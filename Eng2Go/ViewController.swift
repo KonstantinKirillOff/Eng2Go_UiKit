af8643ad33eb3dbc8e5f479e9a44875707ca9483
kirill_off <kirilloffkostya@gmail.com>
2022-10-12 12:27:53 +0600

    Initial Commit

---
+++ Eng2Go/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Eng2Go
+//
+//  Created by Konstantin Kirillov on 12.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
